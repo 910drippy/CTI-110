f4499d98414012759fc054aee389294f47278583
910drippy <60890531+910drippy@users.noreply.github.com>
2020-02-10 11:36:12 -0500

    Add files via upload
    P2T1

---
+++ P2T1_SalesPrediction_EadyDe'Shaw.py
@@ -0,0 +1,18 @@
+# This project is about total sales 
+# 11/7/2019
+# CTI-110 P2T1 - Sales Prediction
+# DeShawn Eady
+#
+
+print=input('Enter the projected sales: ')
+
+# Get the projected total sales.
+total_sales = float(input('Enter the projected sales: '))
+
+#Calculate the profit as 23 percent of total sales.
+profit = total_sales * .23
+
+#Display the profit.
+print('The profit is $230,000', format(profit, ',.2f'))
+      
+      
